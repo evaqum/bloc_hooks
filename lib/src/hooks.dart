65a6da823c2a3dbca734f4a42b95e49609060c79
evaqum <evaqum@internet.ru>
2023-08-30 19:02:50 +0300

    forgot to export one thing oops

--- lib/src/hooks.dart
+++ lib/src/hooks.dart
@@ -3,3 +3,4 @@ export 'hooks/bloc_builder.dart';
 export 'hooks/bloc_consumer.dart';
 export 'hooks/bloc_listener.dart';
 export 'hooks/bloc_selector.dart';
+export 'hooks/bloc_selector_listener.dart';
